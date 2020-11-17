ce2da34c0c8bc052cc7bd0def11cdd3075b110db
faza76 <fazakbar76@gmail.com>
2020-11-17 22:08:35 +0700

    * .gitignore: First Commit
    * ASPNetCore_Web_Movie_List.sln:
    * Program.cs:
    * Startup.cs:
    * appsettings.json:
    * site.js:
    * favicon.ico:
    * site.css:
    * _ViewStart.cshtml:
    * Index.cshtml:
    * ErrorViewModel.cs:
    * _ViewImports.cshtml:
    * Error.cshtml:
    * Privacy.cshtml:
    * _Layout.cshtml:
    * appsettings.Development.json:
    * Index.cshtml:
    * HomeController.cs:
    * LICENSE:
    * launchSettings.json:
    * LICENSE.txt:
    * ASPNetCore_Web_Movie_List.csproj:
    * jquery.js:
    * HellloWorldController.cs:
    * jquery.min.js:
    * jquery.min.map:
    * LICENSE.md:
    * bootstrap.js:
    * bootstrap.css:
    * _ValidationScriptsPartial.cshtml:
    * bootstrap.js.map:
    * bootstrap.min.js:
    * bootstrap.css.map:
    * bootstrap.min.css:
    * bootstrap-grid.css:
    * bootstrap.bundle.js:
    * bootstrap.min.js.map:
    * bootstrap-reboot.css:
    * bootstrap.min.css.map:
    * bootstrap.bundle.js.map:
    * bootstrap-grid.min.css:
    * bootstrap-grid.css.map:
    * LICENSE.txt:
    * bootstrap.bundle.min.js:
    * jquery.validate.js:
    * bootstrap-reboot.min.css:
    * bootstrap-reboot.css.map:
    * additional-methods.js:
    * jquery.validate.min.js:
    * bootstrap.bundle.min.js.map:
    * bootstrap-grid.min.css.map:
    * bootstrap-reboot.min.css.map:
    * additional-methods.min.js:
    * jquery.validate.unobtrusive.js:
    * jquery.validate.unobtrusive.min.js:

---
+++ ASPNetCore_Web_Movie_List/Controllers/HellloWorldController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASPNetCore_Web_Movie_List.Controllers
+{
+    public class HellloWorldController : Controller
+    {
+        // GET: /<controller>/
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Welcome(string name, int number = 1)
+        {
+            return HtmlEncoder.Default.Encode($"this is {name} in {number} action");
+        }
+            
+
+    }
+}
---
+++ ASPNetCore_Web_Movie_List/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPNetCore_Web_Movie_List.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
